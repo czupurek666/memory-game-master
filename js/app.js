f0f28587436c752962a7cf536fd3f8d270db3a58
Karolina_Kantorska <karolina.kantorska@gmail.com>
2018-08-24 18:25:34 +0200

    refactor: function changing a list of cards into array
    function reads am list of hrml objects ant returns an array with classes of children of those object

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
-/*
- * Create a list that holds all of your cards
- */
 
 //  creates an empty array of cards to be suffled
 const cards = [];
+//  creates an empty array of cards that are oppened
 const openCardsClass= [];
 
 //  get a list of <i> elements whose immediate parent element has the class "card"
 //  see dokumentation: https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
 const allCards = document.querySelectorAll('.card > i');
+//const openedCard = document.querySelectorAll('.show > i');
+const openedCard = document.querySelectorAll('.show > i');
 
-// TODO fills the array cards with the class names of i elements
-for (let i= 0; i<allCards.length; i++ ){
-  const singleCardClassName = allCards[i].className;
-  cards.push(singleCardClassName);
-}
-
- //checking if the card array ist filled with the wright information
 /*
-const myPara = document.createElement('i');
-myPara.className = "fa fa-diamond";
-myPara.textContent = ('cards:'+ cards) ;
-document.body.appendChild(myPara);
+* @description changing a list of cards into array of card classes
+* @constructor
+* @param {string} crd - list of cards
+* @param {array} arr - array with a clss Names
 */
+function pushToArray (crd,arr){
+for (let i= 0; i<crd.length; i++ ){
+  const singleCardClassName = crd[i].className;
+  arr.push(singleCardClassName);
+}
+return arr;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -39,43 +39,39 @@ function shuffle(array) {
     return array;
 }
 
-// displayCards function
-// loop through each card and create its HTML
-// add each card's HTML to the page
-
-function displayCards() {
+/*
+* @description loop through each card and create its HTML
+* @description add each card's HTML to the page
+*/
+function displayBlankCards() {
 for (let i= 0; i<allCards.length; i++ ){
   allCards[i].className = cards[i];
 }
-};
+}
+
+
+//display the card's symbol (put this functionality in another function that you call from this one)
+function cardDisplay(evt) {
+  if (evt.target.className === 'card'){
+    evt.target.className = 'card open show';
+    //add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+    // TODO fills the array  with the class names of i elements
+    pushToArray (openedCard, openCardsClass);
+    }
+}
+
+// TODO fills the array cards with the class names of i elements
+pushToArray (allCards, cards);
 
 // TODO shuffle the list of cards using the provided "shuffle" method
 shuffle(cards);
 
 //TODO loop through each card and create its HTML
 //TODO add each card's HTML to the page
-displayCards();
+displayBlankCards();
 
 //TODO event listener for a card. If a card is clicked:
 document.getElementById('deck').addEventListener('click',cardDisplay);
-
-//display the card's symbol (put this functionality in another function that you call from this one)
-function cardDisplay(evt) {
-  if (evt.target.className === 'card'){
-    evt.target.className = 'card open show';
-    addToOpenCards(evt);
-    }
-    //alert ("Hello World!");
-}
-//add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-function addToOpenCards (crd){
-  const openedCard = crd.className;
-  openCardsClass.push(crd);
-  }
-
-  const myPara3 = document.createElement('i');
-  myPara3.textContent = ('cards:'+ openCards);
-  document.body.appendChild(myPara3);
 
 
 /*
